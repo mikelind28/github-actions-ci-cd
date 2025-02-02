0e131bf5a69bb77202f305afc3efce180d4170dc
Mike Lind <mikelind28@gmail.com>
2025-02-02 15:05:49 -0600

    attempt to fix cypress errors

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -19,9 +19,9 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/react18'
+// import { mount } from 'cypress/react'
 
-Cypress.Commands.add('mount', mount)
+// Cypress.Commands.add('mount', mount)
 
 // Example use:
 // cy.mount(<MyComponent />)
